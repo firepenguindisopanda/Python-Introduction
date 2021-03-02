a764ba6d621e0026c56086c86ed4dc3389f663d1
Nicholas <nicosmith.smith3@gmail.com>
2021-03-02 18:49:51 -0400

    Added code to read dna.txt and print to console the output

--- DNA-Alignment/main.cpp
+++ DNA-Alignment/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
+using namespace std;
 
 int main()
 {
+	ifstream fin_dna;
+	string dnaTxt;
+	fin_dna.open("dna.txt");
+	if(!fin_dna.is_open()){
+		cout<<"Couldn't find dna.txt file....\nClosing program.";
+		return 0;
+	}
+	while(getline(fin_dna, dnaTxt)){
+		cout<<dnaTxt;
+	}
+	
     return 0;
 }
