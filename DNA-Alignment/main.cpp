bfca17494d588825e49e0db43b9d5346cc03397d
Nicholas <nicosmith.smith3@gmail.com>
2021-03-03 17:48:04 -0400

    Added code to read patterns.txt file

--- DNA-Alignment/main.cpp
+++ DNA-Alignment/main.cpp
@@ -7,14 +7,24 @@ using namespace std;
 int main()
 {
 	ifstream fin_dna;
+	ifstream fin_patterns;
 	string dnaTxt;
+	string patternsTxt;
 	fin_dna.open("dna.txt");
+	fin_patterns.open("patterns.txt");
 	if(!fin_dna.is_open()){
 		cout<<"Couldn't find dna.txt file....\nClosing program.";
 		return 0;
 	}
+	if(!fin_patterns.is_open()){
+		cout<<"Couldn't find patterns.txt.....\nClosing program.";
+		return 0;
+	}
 	while(getline(fin_dna, dnaTxt)){
 		cout<<dnaTxt;
+	}
+	while(getline(fin_patterns, patternsTxt)){
+		cout<<patternsTxt<<"\n";
 	}
 	
     return 0;
