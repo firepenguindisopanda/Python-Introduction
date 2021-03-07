e490b812fb0a742cf691ac7284c5baf10b77c2c5
Nicholas <nicosmith.smith3@gmail.com>
2021-03-06 23:26:39 -0400

    Read from files and made sure it printed it to the screen

--- Blocking-Websites/main.cpp
+++ Blocking-Websites/main.cpp
@@ -2,12 +2,18 @@
 #include <fstream>
 #include <iomanip>
 #include <cstdlib>
-
+using namespace std;
 int main()
 {
 	ifstream fin_dns;
 	ifstream fin_queries;
 	ifstream fin_blacklist;
+	string dnsTxt;
+	string queriesTxt;
+	string blacklistTxt;
+	fin_dns.open("dns.txt");
+	fin_queries.open("queries.txt");
+	fin_blacklist.open("blacklist.txt");
 	if(!fin_dns.is_open()){
 		cout<<"Couldn't find dns.txt file....\nClosing program.";
 		return 0;
@@ -19,6 +25,15 @@ int main()
 	if(!fin_blacklist.is_open()){
 		cout<<"Couldn't find blacklist file....\nClosing program.";
 		return 0;
+	}
+	while(getline(fin_dns, dnsTxt)){
+		cout<<dnsTxt<<"\n";
+	}
+	while(getline(fin_queries, queriesTxt)){
+		cout<<queriesTxt<<"\n";
+	}
+	while(getline(fin_blacklist, blacklistTxt)){
+		cout<<blacklistTxt<<"\n";
 	}
     return 0;
 }
