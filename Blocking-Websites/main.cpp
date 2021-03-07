8aecc488ebd07aea118205756a3d7ea986fb4041
Nicholas <nicosmith.smith3@gmail.com>
2021-03-06 23:11:59 -0400

    added checks for file reading

--- Blocking-Websites/main.cpp
+++ Blocking-Websites/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
 
 int main()
 {
+	ifstream fin_dns;
+	ifstream fin_queries;
+	ifstream fin_blacklist;
+	if(!fin_dns.is_open()){
+		cout<<"Couldn't find dns.txt file....\nClosing program.";
+		return 0;
+	}
+	if(!fin_queries.is_open()){
+		cout<<"Couldn't find queries file....\n closing program.";
+		return 0;
+	}
+	if(!fin_blacklist.is_open()){
+		cout<<"Couldn't find blacklist file....\nClosing program.";
+		return 0;
+	}
     return 0;
 }
