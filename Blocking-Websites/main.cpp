20d1fc0a5377fb3249214c0aacbd2c5b67c15d22
Nicholas <nicosmith.smith3@gmail.com>
2021-03-11 22:00:08 -0400

    updated the first while to store the data into a struct DnsData

--- Blocking-Websites/main.cpp
+++ Blocking-Websites/main.cpp
@@ -3,14 +3,20 @@
 #include <iomanip>
 #include <cstdlib>
 using namespace std;
+struct DnsData{
+	string siteName[50];
+	string ipAddress[50];
+};
 int main()
 {
 	ifstream fin_dns;
 	ifstream fin_queries;
 	ifstream fin_blacklist;
-	string dnsTxt;
+	string dnsTxt, site, ip;
 	string queriesTxt;
 	string blacklistTxt;
+	int dnsCount = 0;
+	DnsData dd;
 	fin_dns.open("dns.txt");
 	fin_queries.open("queries.txt");
 	fin_blacklist.open("blacklist.txt");
@@ -26,14 +32,28 @@ int main()
 		cout<<"Couldn't find blacklist file....\nClosing program.";
 		return 0;
 	}
-	while(getline(fin_dns, dnsTxt)){
-		cout<<dnsTxt<<"\n";
+	fin_dns>>site;
+	fin_dns>>ip;
+	while(!fin_dns.eof()){
+		dd.siteName[dnsCount] = site;
+		dd.ipAddress[dnsCount] = ip;
+		cout<<site<<"\n";
+		cout<<ip<<"\n";
+		fin_dns>>site;
+		fin_dns>>ip;
+		dnsCount += 1;
 	}
+	cout<<"\n";
 	while(getline(fin_queries, queriesTxt)){
 		cout<<queriesTxt<<"\n";
 	}
+	cout<<"\n";
 	while(getline(fin_blacklist, blacklistTxt)){
 		cout<<blacklistTxt<<"\n";
 	}
-    return 0;
+	//Checks if the data was entered correctly
+//		for(int i =0; i < dnsCount; i++){
+//			cout<<dd.siteName[i]<<"\t\t"<<dd.ipAddress[i]<<"\n";
+//		}
+//	    return 0;
 }
